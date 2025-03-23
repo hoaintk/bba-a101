bd6a547cd9651788788b04aa4f72503323d7ab49
hoaintk <hoaingkh@gmail.com>
2025-03-23 13:31:13 +0700

    chore: rearrange files

--- lesson-04/01-setup.js
+++ lesson-04/01-setup.js
@@ -1,5 +1,5 @@
 let missionObjective = "Khám phá và kiểm thử API"
-const crew = ["Nam", "Lan", "Hùng"]
+const crew = ["Tân", "Hoài", "Tú"]
 console.log(
   "Hệ thống API Explorer đã sẵn sàng! Phi hành đoàn: " +
     crew +
