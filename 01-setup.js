8ba5d3338569fb0dc367d334a3b48c88d163531e
hoaintk <hoaingkh@gmail.com>
2025-03-23 13:01:00 +0700

    Test01_API

---
+++ 01-setup.js
@@ -0,0 +1,8 @@
+let missionObjective = "Khám phá và kiểm thử API"
+const crew = ["Nam", "Lan", "Hùng"]
+console.log(
+  "Hệ thống API Explorer đã sẵn sàng! Phi hành đoàn: " +
+    crew +
+    " sẽ cùng bạn thực hiện nhiệm vụ " +
+    missionObjective,
+)
---
+++ 02-reason.js
@@ -0,0 +1,10 @@
+let apiDefinition = "API là phương thức giao tiếp giữa Client và Server"
+const whyTestAPI = [
+  "Đảm bảo tính đúng đắn",
+  "Kiểm tra hiệu suất",
+  "Phát hiện lỗi sớm",
+]
+
+for (let i = 0; i < whyTestAPI.length; i++) {
+  console.log("Lý do " + (i + 1) + ": " + whyTestAPI[i])
+}
